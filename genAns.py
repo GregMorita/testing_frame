10ef15b7d0f31f013c64417bcf2eb1002c181f24
gregorymorita <gmorita2003@gmail.com>
2023-10-23 11:21:42 -0700

    added readme

---
+++ genAns.py
@@ -0,0 +1,11 @@
+import sys
+
+
+#generate ans
+def main():
+    total = len(sys.argv)
+    
+
+
+if __name__ == "__main__":
+    main()
--- testing.py
+++ testing.py
@@ -2,7 +2,6 @@ import subprocess
 import sys
 import glob
 import getopt
-import typing
 
 options = "rhv:"
 arglst = sys.argv[1:]
@@ -12,16 +11,19 @@ opt = ""
 Simple python script to help test c code
 compiles and runs code on a test dir
 by default, takes stdout of program on testfile and diffs with expected
+requires installation of gcc, python 3, and valgrind
 """
 
+#compiles program with all warnings
 def compile(program: str):
     subprocess.run(["gcc -Wall -pedantic -std=c99", program, "-g"])
 
+#tests for mem leaks
 def valgrind(program: str, args: list):
     pass
 
 def main(): 
-    if (len(sys.argv) < 3):
+    if (len(sys.argv) < 4 and len(sys.argv) > 2):
         print("improper amount of arguments, try testing.py -h")
     
     try:
@@ -30,9 +32,9 @@ def main():
         for currentArgument in arguments:
     
             if currentArgument in ("-h"):
-                print("Usage: testing.py [-r Testing_dir] [-hv] [program args] Program_path Test_case_dir Testcase_answer_dir ")
-                print("r: runs files in Testing_dir, passes in testdir filepath use for smaller, incremental tests") 
-                print("h: displays this message")
+                print("Usage: testing.py [-hvr] Testcase_dir Testcase_ans_dir Program_path [Program_args]")
+                print("r: runs files in Testcase_dir, passes in Testcase_ans_dir filepath for use on smaller, incremental tests") 
+                print("h: displays this message. v: runs valgrind on program to check for memLeaks")
                 exit(0)
 
             elif currentArgument in ("-r"):
@@ -45,10 +47,10 @@ def main():
         print(str(err))
     
     total_args = len(sys.argv[0])
-    prog = sys.argv[total_args - 3]
+
     #just a lil' bit fucked
     if val:
-        pass
+        valgrind()
     else:
         compile(prog)
 
