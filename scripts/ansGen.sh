25e3f0ab08d9c08f4a2bfdc48b491443677a4e55
gregorymorita <gmorita2003@gmail.com>
2023-10-25 11:10:07 -0700

    finished shell scripts

--- scripts/ansGen.sh
+++ scripts/ansGen.sh
@@ -3,17 +3,21 @@ COUNT=0
 
 # uses $1 for the program to be used
 
-for DIR in ./*tests
-do
-    if [ ! -d $DIR ]; then
-        echo "testing directory doesn't exist"
-        exit
-    fi
+if [ $# -gt 0 ]; then
+    for DIR in ./*tests
+    do
+        if [ ! -d $DIR ]; then
+            echo "testing directory doesn't exist"
+            exit
+        fi
 
-    echo this is $DIR:
-    for file in ${DIR}/*.txt
-    do 
-        echo $file
+        echo this is $DIR:
+        for file in ${DIR}/*.txt
+        do 
+            SUFF=".txt"
+            ANS=${file%"$SUFF"}_ans.txt
+            touch ${ANS}
+            $1 $file >> $ANS
+        done
     done
-done
-
+fi
